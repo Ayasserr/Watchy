6acb6ad82fa87e3d073ae419599f95fe71c11f09
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2022-08-13 00:11:19 +0200

    Add background colour and text colour of the app

---
+++ Watchy/Extension/Color+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extension.swift
+//  Watchy
+//
+//  Created by Abdelrahman Abo Al Nasr on 13/08/2022.
+//
+
+import SwiftUI
+
+extension Color {
+	/// The background color of discover screen.
+	public static let backgroundColor: Color = Color("Background Color")
+	
+	/// The color of all texts and labels in the app.
+	public static let textColor: Color = Color("Text Color")
+}
