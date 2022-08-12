1c51cbabb4841250a537bd66ec07c22b53fb1ae5
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2022-08-13 00:02:06 +0200

    Create TabView

--- Watchy/ContentView.swift
+++ Watchy/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+	@State private var selectedTab: Tabs = .discover
+	
+	var body: some View {
+		TabView(selection: $selectedTab) {
+			Text("Home")
+				.tabItem { self.tabLabel(text: "Home", image: "house") }
+			
+			Text("Search")
+				.tabItem { self.tabLabel(text: "Search", image: "magnifyingglass") }
+		}
     }
+	
+	@ViewBuilder
+	private func tabLabel(text: String, image: String) -> some View {
+		VStack {
+			Image(systemName: image).imageScale(.large)
+			Text(text)
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+enum Tabs {
+	case discover
+	case search
 }
