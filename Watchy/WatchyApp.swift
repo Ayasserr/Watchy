68be586fdb6c9d2f1017bed864e9457d8ec80d95
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2022-08-12 23:57:26 +0200

    Initial Commit

---
+++ Watchy/WatchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchyApp.swift
+//  Watchy
+//
+//  Created by Abdelrahman Abo Al Nasr on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct WatchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
