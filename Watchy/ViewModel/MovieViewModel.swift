9bba34de21f2d03de42c4009f07a4af25c8e9a2e
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2022-08-13 15:43:45 +0200

    Add MovieViewModel

---
+++ Watchy/ViewModel/MovieViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  MovieViewModel.swift
+//  Watchy
+//
+//  Created by Abdelrahman Abo Al Nasr on 13/08/2022.
+//
+
+import Foundation
+
+@MainActor
+class MovieViewModel: ObservableObject, RequestDelegate {
+	@Published var movie: MovieDetails?
+	@Published var cast: [Cast] = []
+	@Published var genres: [MovieGenres] = []
+	
+	let movieID: Int?
+	
+	init(movieID: Int) {
+		self.movieID = movieID
+	}
+	
+	public func getMovieDetails() async throws {
+		try await getMovie()
+		try await getCast()
+	}
+	
+	private func getMovie() async throws {
+		let result = try await sendRequest(from: MoviesServicesEndpoint.movie(id: self.movieID!), model: MovieDetails.self)
+		self.movie = result
+	}
+	
+	private func getCast() async throws {
+		let result = try await sendRequest(from: MoviesServicesEndpoint.cast(id: self.movieID!), model: CastResponse.self)
+		self.cast = result.cast
+	}
+}
