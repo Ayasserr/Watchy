226b836de879d34a4d7532e99ba5360e9b465818
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2022-08-13 02:36:34 +0200

    Add hex code initializer for Color type

--- Watchy/Extension/Color+Extension.swift
+++ Watchy/Extension/Color+Extension.swift
@@ -7,6 +7,37 @@
 
 import SwiftUI
 
+// MARK: - Color HexCode Initalize
+extension Color {
+	/// Initialize a new Color from its hexadecimal code.
+	///
+	/// - Parameters:
+	///   - colorSpace:
+	///   - hecCode:
+	///
+	/// - Note: If you are going to use this color often in the project, better to add the color to the project assets.
+	public init(
+		_ colorSpace: Color.RGBColorSpace = .displayP3,
+		hexCode: String
+	) {
+		let scanner = Scanner(string: hexCode)
+		var rgbValues: UInt64 = 0
+		scanner.scanHexInt64(&rgbValues)
+		
+		let r = (rgbValues & 0xff0000) >> 16
+		let g = (rgbValues & 0xff00) >> 8
+		let b = rgbValues & 0xff
+		
+		self.init(
+			colorSpace,
+			red: Double(r) / 255,
+			green: Double(g) / 255,
+			blue: Double(b) / 255
+		)
+	}
+}
+
+// MARK: - Main Colors
 extension Color {
 	/// The background color of discover screen.
 	public static let backgroundColor: Color = Color("Background Color")
