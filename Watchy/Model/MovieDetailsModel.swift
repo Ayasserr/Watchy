95986f57daa8faab5b889db029973089a9c50406
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2022-08-13 02:28:42 +0200

    Add MovieDetails model

---
+++ Watchy/Model/MovieDetailsModel.swift
@@ -0,0 +1,51 @@
+//
+//  MovieDetailsModel.swift
+//  Watchy
+//
+//  Created by Abdelrahman Abo Al Nasr on 13/08/2022.
+//
+
+import Foundation
+
+struct MovieDetails: Codable, Identifiable {
+	let id: Int
+	let title: String
+	let overview: String?
+	let genres: [MovieGenres]
+	let posterPath: String?
+	let releaseDate: String
+	let runtime: Int?
+	let status: MovieStatus
+	let voteAverage: Double
+	
+	enum CodingKeys: String, CodingKey {
+		case id
+		case title
+		case overview
+		case genres
+		case posterPath = "poster_path"
+		case releaseDate = "release_date"
+		case runtime
+		case status
+		case voteAverage = "vote_average"
+	}
+}
+
+extension MovieDetails {
+	enum MovieStatus: String, Codable {
+		case rumored = "Rumored"
+		case planned = "Planned"
+		case inProduction = "In Production"
+		case postProduction = "Post Production"
+		case released = "Released"
+		case canceled = "Canceled"
+	}
+}
+
+extension MovieDetails {
+	var releaseDateFormatted: Date {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "yyyy-MM-dd"
+		return dateFormatter.date(from: self.releaseDate) ?? Date()
+	}
+}
