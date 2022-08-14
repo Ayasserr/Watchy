bdb7b3153329fcf2e5d3bc93de3c2677008d5285
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2022-08-14 11:11:49 +0200

    Removing genres background colours

--- Watchy/Model/MovieGenre.swift
+++ Watchy/Model/MovieGenre.swift
@@ -12,39 +12,3 @@ struct MovieGenres: Codable, Identifiable, Hashable {
 	let id: Int
 	let name: String
 }
-
-extension MovieGenres {
-	var color: Color {
-		switch name {
-			case "Action": 			return .red
-			case "Adventure": 		return .yellow
-			case "Animation": 		return .orange
-				
-			case "Comedy":			return .green
-			case "Crime":			return .brown
-				
-			case "Documentary":		return .brown
-			case "Drama":			return .eggplant
-				
-			case "Family":			return .dodgerBlue
-			case "Fantasy":			return .teal
-				
-			case "History":			return .blackCoffee
-			case "Horror":			return .black
-				
-			case "Music":			return .cyan
-			case "Mystery":			return .gray
-				
-			case "Romance":			return .pink
-			case "Science Fiction":	return .emerald
-				
-			case "TV Movie":		return .indigo
-			case "Thriller":		return .purple
-				
-			case "War":				return .black
-			case "Western":			return .brown
-				
-			default:				return .gray.opacity(0.5)
-		}
-	}
-}
