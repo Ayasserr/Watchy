4107fc3ae53bf0d2df3c5ea824367b249a821460
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2022-08-15 13:22:57 +0200

    Change RateView design

--- Watchy/Subview/RateView.swift
+++ Watchy/Subview/RateView.swift
@@ -11,38 +11,61 @@ struct RateView: View {
 	let voteAverage: Double
 	
     var body: some View {
-		ZStack {
-			Circle()
-				.trim(from: 0, to: voteAverage / 10)
-				.stroke(rateColor, style: StrokeStyle(lineWidth: 4, lineCap: .round))
-				.rotationEffect(.degrees(-90))
-			
-			Circle()
-				.stroke(.white.opacity(0.3), lineWidth: 4)
-				
-			
-			Text(String(format: "%0.2f", voteAverage))
-				.font(.subheadline)
+		let stars = HStack(spacing: 10) {
+			ForEach(0 ..< 5, id: \.self) { index in
+				Image(systemName: "star.fill")
+					.resizable()
+					.aspectRatio(contentMode: .fit)
+			}
 		}
-		.frame(width: 50, height: 50)
+		
+		stars.overlay {
+			GeometryReader { gemoetry in
+				let width = voteAverage / 5 * gemoetry.size.width
+				ZStack(alignment: .center) {
+					Rectangle()
+						.frame(width: width)
+						.foregroundColor(.yellow)
+				}
+			}
+			.mask(stars)
+		}
+		.foregroundColor(.yellow.opacity(0.3))
 	}
-	
-	private var rateColor: Color {
-		switch voteAverage {
-			case 0 ..< 2.5: return .red
-			case 2.5 ..< 5: return .orange
-			case 5 ..< 7.5: return .yellow
-			case 7.5... : return .green
-			default: return .gray
-		}
-    }
 }
 
 struct RateView_Previews: PreviewProvider {
     static var previews: some View {
-		RateView(voteAverage: 7.5)
+		RateView(voteAverage: 7.5 / 2)
 			.previewLayout(.sizeThatFits)
 			.padding()
-			.background(.gray)
     }
 }
+
+/*
+ ZStack {
+	 Circle()
+		 .trim(from: 0, to: voteAverage / 10)
+		 .stroke(rateColor, style: StrokeStyle(lineWidth: 4, lineCap: .round))
+		 .rotationEffect(.degrees(-90))
+	 
+	 Circle()
+		 .stroke(.white.opacity(0.3), lineWidth: 4)
+		 
+	 
+	 Text(String(format: "%0.2f", voteAverage))
+		 .font(.subheadline)
+		 .fontWeight(.medium)
+ }
+ .frame(width: 50, height: 50)
+ 
+ private var rateColor: Color {
+	 switch voteAverage {
+		 case 0 ..< 2.5: return .red
+		 case 2.5 ..< 5: return .orange
+		 case 5 ..< 7.5: return .yellow
+		 case 7.5... : return .green
+		 default: return .gray
+	 }
+ }
+ */
